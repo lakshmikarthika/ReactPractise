08da80d76ee0d40d642e1b7a3726533b01938ab6
GTR1995 <y12ame435@gmail.com>
2022-07-17 10:58:54 +0530

    Added sample redux toolkit project

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {useDispatch} from 'react-redux'
+import {login,logout} from '../features/user'
+
+function Login() {
+    const dispatch = useDispatch();
+  return (
+    <div>
+        <button onClick={()=> {dispatch(login({name:'Karthi',age:20,email:'k@gmail.com'}))}}>Login</button>
+        <button onClick={()=> {dispatch(logout())}}>Logout</button>
+    </div>
+  )
+}
+
+export default Login
