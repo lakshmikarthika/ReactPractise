79b5f2b118a9eaba45ed9585e2ea231a649f15db
karthika <49395313+lakshmikarthika@users.noreply.github.com>
2022-11-17 16:16:46 +0530

    Update Child.js

--- Child2ParentCommu/Child.js
+++ Child2ParentCommu/Child.js
@@ -1,10 +1,20 @@
-import React, { useState } from 'react';
-
-function Child({onclick}) {
+import { useState } from "react";
+export default function Child({ getData }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const clickHandler = (e) => {
+    e.preventDefault();
+    getData({ username, password });
+  };
   return (
-    <div >
-      <button onClick={onclick}>Increment</button>
-    </div>
+    <>
+      <form>
+        <label>Username</label>
+        <input type="text" onChange={(e) => setUsername(e.target.value)} />
+        <label>Password</label>
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={clickHandler}>Login</button>
+      </form>
+    </>
   );
 }
-export default Child;
